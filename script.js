cee2aac8263610a39489f66b29ed9a491a2b753e
Jack Markwalter <jackmarkwalter3@gmail.com>
2020-01-01 22:41:30 -0500

    cleaning up code

--- script.js
+++ script.js
@@ -36,23 +36,25 @@ const choiceA = document.getElementById("A");
 const choiceB = document.getElementById("B");
 const choiceC = document.getElementById("C");
 const choiceD = document.getElementById("D");
+const choice1 = document.getElementById("1");
+const choice2 = document.getElementById("2");
+const choice3 = document.getElementById("3");
+const choice4 = document.getElementById("4");
 const progress = document.getElementById("progress");
 const scoreDiv = document.getElementById("score");
+const startButton = document.getElementById("startButton");
 
-var score = 0;
 const lastQuestion = questions.length - 1;
 var runningQuestion = 0;
 
 
 function showQuestion() {
     var q = questions[runningQuestion];
-    qImg.innerHTML = "<img src=" + q.imgSrc + ">";
     question.innerHTML = "<p>" + q.title + "</p>";
-    //I will need to update the below to match my matrix...
-    choiceA.innerHTML = q.choices[0];
-    choiceB.innerHTML = q.choices[1];
-    choiceC.innerHTML = q.choices[2];
-    choiceD.innerHTML = q.choices[3]; 
+    choice1.innerHTML = q.choices[0];
+    choice2.innerHTML = q.choices[1];
+    choice3.innerHTML = q.choices[2];
+    choice4.innerHTML = q.choices[3]; 
 }
 
 
@@ -72,19 +74,14 @@ function showProgress() {
     }
 }
 
-var count = 0
-var questionTime = 10;
-var gaugeWidth  = 150;
-var gaugeUnit = gaugeWidth / questionTime;
+var count = 75
 
 function showCounter() {
-    if(count <= questionTime) {
+    if(count >= 0) {
         counter.innerHTML = count;
-        timeGauge.style.width = count * gaugeUnit;
-        count++;
+        count--;
     }
     else {
-        count = 0;
         answerIsWrong();
         if (runningQuestion < lastQuestion) {
             runningQuestion++;
@@ -98,16 +95,13 @@ function showCounter() {
 }
 
 function checkAnswer(answer) {
-    if (answer == questions[runningQuestion].answer) {
-        answerIsCorrect();
-        score++
-        console.log(score)
+    if (answer.innerHTML == questions[runningQuestion].answer) {
+        console.log(count);
     }
     else {
-        answerIsWrong()
+        answerIsWrong();
     }
     if (runningQuestion < lastQuestion) {
-        count = 0;
         runningQuestion++;
         showQuestion();
     }
@@ -120,22 +114,18 @@ function checkAnswer(answer) {
 
 function showScore() {
     scoreDiv.style.display = "block"
-    var scorePerCent = Math.round(100 * (score / questions.length));
-    scoreDiv.innerHTML = "<p>" + scorePerCent + "%</p>";
+    var scorePerCent = count;
+    scoreDiv.innerHTML = "<p>" + scorePerCent + "</p>";
 
 }
 
-function answerIsCorrect() {
-    document.getElementById(runningQuestion).style.backgroundColor = "#0f0";
-}
 
 function answerIsWrong() {
-    document.getElementById(runningQuestion).style.backgroundColor = "#f00";
+    count = count - 10;
 }
 
-start.addEventListener("click", startQuiz);
-choiceA.addEventListener("click", checkAnswer);
-choiceB.addEventListener("click", checkAnswer);
-choiceC.addEventListener("click", checkAnswer);
-choiceD.addEventListener("click", checkAnswer);
-    
+startButton.addEventListener("click", startQuiz);
+choice1.addEventListener("click", checkAnswer);
+choice2.addEventListener("click", checkAnswer);
+choice3.addEventListener("click", checkAnswer);
+choice4.addEventListener("click", checkAnswer);
