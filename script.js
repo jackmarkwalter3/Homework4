b5a58ad47f15447322cd3fb0cb98d9c4296b1246
Jack Markwalter <jackmarkwalter3@gmail.com>
2020-01-05 22:06:02 -0500

    fixed mc errors

--- script.js
+++ script.js
@@ -32,10 +32,10 @@ const qImg = document.getElementById("qImage");
 const question = document.getElementById("question");
 const counter = document.getElementById("counter");
 const timeGauge = document.getElementById("timeGauge");
-const choiceA = document.getElementById("A");
-const choiceB = document.getElementById("B");
-const choiceC = document.getElementById("C");
-const choiceD = document.getElementById("D");
+var choiceA = $("#A");
+var choiceB = $("#B");
+var choiceC = $("#C");
+var choiceD = $("#D");
 const choice1 = document.getElementById("1");
 const choice2 = document.getElementById("2");
 const choice3 = document.getElementById("3");
@@ -44,17 +44,17 @@ const progress = document.getElementById("progress");
 const scoreDiv = document.getElementById("score");
 const startButton = document.getElementById("startButton");
 
-const lastQuestion = questions.length - 1;
+const lastQuestion = questions.length;
 var runningQuestion = 0;
 
 
 function showQuestion() {
     var q = questions[runningQuestion];
     question.innerHTML = "<p>" + q.title + "</p>";
-    choice1.innerHTML = q.choices[0];
-    choice2.innerHTML = q.choices[1];
-    choice3.innerHTML = q.choices[2];
-    choice4.innerHTML = q.choices[3]; 
+    choiceA.text(q.choices[0]);
+    choiceB.text(q.choices[1]);
+    choiceC.text(q.choices[2]);
+    choiceD.text(q.choices[3]); 
 }
 
 
@@ -63,69 +63,97 @@ function startQuiz() {
     start.style.display = "none";
     showQuestion();
     quiz.style.display = "block";
-    showProgress();
-    showCounter();
+    // showProgress();
+    // showCounter();
     TIMER = setInterval(showCounter, 1000);
 }
 
-function showProgress() {
-    for (var i = 0; i <= lastQuestion; i++) {
-        progress.innerHTML += "<div class='prog' id=" + i + "></div>";
-    }
-}
+// function showProgress() {
+//     for (var i = 0; i <= lastQuestion; i++) {
+//         progress.innerHTML += "<div class='prog' id=" + i + "></div>";
+//     }
+// }
 
 var count = 75
 
 function showCounter() {
-    if(count >= 0) {
+    if(count >= 0 && runningQuestion < lastQuestion) {
         counter.innerHTML = count;
         count--;
     }
     else {
-        answerIsWrong();
         if (runningQuestion < lastQuestion) {
             runningQuestion++;
             showQuestion();
         }
         else {
             clearInterval(TIMER);
-            showScore();
+            showScoreBoard();
         }
     }
 }
 
-function checkAnswer(answer) {
-    if (answer.innerHTML == questions[runningQuestion].answer) {
-        console.log(count);
-    }
-    else {
-        answerIsWrong();
-    }
-    if (runningQuestion < lastQuestion) {
-        runningQuestion++;
-        showQuestion();
-    }
-    else {
-        clearInterval(TIMER);
-        showScore();
-    }
+
+var highScores = [];
+
+
+// function checkAnswer(answer) {
+//     if (answer.innerHTML == questions[runningQuestion].answer) {
+//         console.log(count);
+//     }
+//     else {
+//         answerIsWrong();
+//     }
+//     if (runningQuestion < lastQuestion) {
+//         runningQuestion++;
+//         showQuestion();
+//     }
+//     else {
+//         clearInterval(TIMER);
+//         showScore();
+//     }
     
+// }
+
+function showScoreBoard() {
+    scoreDiv.style.display = "block";
+    $("#insertScore").text(count);
+    quiz.style.display = "none";
+    counter.innerHTML = "";
 }
 
-function showScore() {
-    scoreDiv.style.display = "block"
-    var scorePerCent = count;
-    scoreDiv.innerHTML = "<p>" + scorePerCent + "</p>";
 
-}
-
-
-function answerIsWrong() {
-    count = count - 10;
-}
+// function answerIsWrong() {
+//     count = count - 10;
+// }
 
 startButton.addEventListener("click", startQuiz);
-choice1.addEventListener("click", checkAnswer);
-choice2.addEventListener("click", checkAnswer);
-choice3.addEventListener("click", checkAnswer);
-choice4.addEventListener("click", checkAnswer);
+
+$(".choice").click(function(){
+    var answer = $(this).text();
+    console.log(answer);
+    if(answer === questions[runningQuestion].answer){
+        runningQuestion++;
+        if(runningQuestion < lastQuestion){
+            showQuestion();
+        }
+        else {
+            showScoreBoard();
+        }
+    }
+    else {
+        count = count - 10;
+        runningQuestion++;
+        if(runningQuestion < lastQuestion){
+            showQuestion();
+        }
+        else {
+            showScoreBoard();
+        }
+    }
+})
+
+// choice1.addEventListener("click", checkAnswer);
+// choice2.addEventListener("click", checkAnswer);
+// choice3.addEventListener("click", checkAnswer);
+// choice4.addEventListener("click", checkAnswer);
